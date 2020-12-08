eb705914297d62eaf5c20ceb32e5a13f05e4f8ce
Parth17071999 <56356024+Parth17071999@users.noreply.github.com>
2020-12-08 19:51:37 +0530

    initalcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NoMatch from './NoMatch';
+import Pilot from './Pilot';
+import PilotRegister from './PilotRegister';
+import PilotOp from './PilotOp';
+import PilotRequestPermission from './PilotRequestPermission';
+import PilotRequestInformation from './PilotRequestInformation';
+import RequestSent from './RequestSent';
+import Government from './Government';
+import GovernmentRegister from './GovernmentRegister';
+import Insurance from './Insurance';
+import InsuranceRegister from './InsuranceRegister';
+import InsuranceOp from './InsuranceOp';
+import InsuranceRequestInformation from './InsuranceRequestInformation';
+import Layout from './components/Layout';
+import NavigationBar from './components/NavigationBar';
+import Jumbotron from './components/Jumbotron';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Jumbotron />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/pilot" component={Pilot} />
+              <Route path="/pilotRegister" component={PilotRegister} />
+              <Route path="/pilotop" component={PilotOp} />
+              <Route path="/pilotrequestpermission" component={PilotRequestPermission} />
+              <Route path="/pilotrequestinformation" component={PilotRequestInformation} />
+              <Route path="/requestsent" component={RequestSent} />
+              <Route path="/government" component={Government} />
+              <Route path="/governmentRegister" component={GovernmentRegister} />
+              <Route path="/insurance" component={Insurance} />
+              <Route path="/insuranceRegister" component={InsuranceRegister} />
+              <Route path="/insuranceop" component={InsuranceOp} />
+              <Route path="/insurancerequestinformation" component={InsuranceRequestInformation} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Home() {
+    return (
+        <div>
+            <h2>Hello World!</h2>
+            <p>This is the home page</p>
+        </div>
+    )
+}
+export default Home
